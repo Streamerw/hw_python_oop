94377c4be6f9bc5db853207d2185f95d6f164a87
streamer w <streamer.work@gmail.com>
2020-07-27 14:15:07 +0000

    classes

--- homework.py
+++ homework.py
@@ -2,11 +2,32 @@ import datetime as dt
 
 
 class Calculator:
-    pass
+    def __init__(self, limit):
+        self.limit = limit
+        pass
+    
+    def add_record():
+        pass
+    
+    def get_today_stats():
+        pass
+    def get_week_stats():
+        pass
+    
 
+class CashCalculator(Calculator):
+    def get_today_cash_remained():
+         pass
+
+class CaloriesCalculator(Calculator):
+    def get_calories_remained():
+        pass
 
 class Record:
     pass
+
+cash_calculator = CashCalculator(1000)
+print(cash_calculator)
 
 print("change")
 ####################
